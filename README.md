634098e22b66b0e5dfb1e70c6cf4f37d0bc71109
Prarthana Nirhali <72865717+PrarthanaN@users.noreply.github.com>
2020-12-28 15:43:36 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Project-34
-This is the code of Project 34 (Newton's Cradle)
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ Rope.js
@@ -0,0 +1,25 @@
+class Rope{
+    constructor(bodyA, bodyB, offsetX, offsetY){
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+        var rope_options = {
+            bodyA: bodyA,
+            bodyB: bodyB,
+            pointB: {x: this.offsetX, y: this.offsetY},
+            //stiffiness: 0.05,
+            //length: 350
+        }
+        this.rope = Constraint.create(rope_options);
+        World.add(world, this.rope);
+    }
+    display(){
+        var pointA = this.rope.bodyA.position;
+        var pointB = this.rope.bodyB.position;
+        strokeWeight(4);
+        stroke("white");
+        var pointC = pointB.x + this.offsetX;
+        var pointD = pointB.y + this.offsetY;
+        //fill("white");
+        line(pointA.x, pointA.y, pointC, pointD);
+    }
+}
